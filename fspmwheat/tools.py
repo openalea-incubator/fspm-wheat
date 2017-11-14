c14fba90319c992510332c1ecf86161ff57d0fe0
cchambon <20965772+cachambon@users.noreply.github.com>
2017-11-14 09:20:07 +0000

    Move graphs generic routines and MTG dependencies from CN-Wheat to FSPM-Wheat.

--- fspmwheat/tools.py
+++ fspmwheat/tools.py
@@ -20,7 +20,16 @@
         $Id$
 """
 
+import os
+
+import numpy as np
 import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+
+from alinea.adel.mtg import to_plantgl
+from openalea.plantgl.all import Viewer,Vector3
+
 
 def combine_dataframes_inplace(model_dataframe, shared_column_indexes, shared_dataframe_to_update):
     """Combine `model_dataframe` and `shared_dataframe_to_update` in-place:
@@ -81,3 +90,121 @@ def combine_dataframes_inplace(model_dataframe, shared_column_indexes, shared_da
     for column in new_shared_dataframe.columns:
         shared_dataframe_to_update[column] = new_shared_dataframe[column]
     shared_dataframe_to_update.reset_index(0, drop=True, inplace=True)
+    
+    
+def plot_linear_regression(x_array, y_array, x_label='x', y_label='y', plot_filepath=None):
+    """Perform a linear regression of `x_array` vs `y_array`
+    and create a plot showing the fit against the original data.
+    If `plot_filepath` is not None, save the plot to a PNG file. Otherwise display the plot.
+
+    This is derived from http://learningzone.rspsoc.org.uk/index.php/Learning-Materials/Python-Scripting/6.4-Fitting-linear-equations,
+    which is under license CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US).
+
+    :Parameters:
+
+        - `x_array` (:class:`numpy.ndarray`) - The first set of measurements.
+
+        - `y_array` (:class:`numpy.ndarray`) - The second set of measurements.
+
+        - `x_label` (:class:`str`) - The label of the abscissa axis. Default is 'x'.
+
+        - `y_label` (:class:`str`) - The label of the ordinates axis. Default is 'y'.
+
+        - `plot_filepath` (:class:`str`) - The file path to save the plot in.
+            If `None`, do not save the plot.
+
+    :Examples:
+
+    >>> import pandas as pd
+    >>> modelmaker_output_df = pd.read_csv('modelmaker_output.csv') # 'modelmaker_output.csv' must contain at least the column 'Sucrose_Phloem'
+    >>> cnwheat_output_df = pd.read_csv('cnwheat_output.csv') # 'cnwheat_output.csv' must contain at least the column 'Sucrose_Phloem'
+    >>> plot_linear_regression(modelmaker_output_df.Sucrose_Phloem,
+                               cnwheat_output_df.Sucrose_Phloem,
+                               x_label='modelmaker_{}'.format('Sucrose_Phloem'),
+                               y_label='cnwheat_{}'.format('Sucrose_Phloem'),
+                               plot_filepath='compare.png')
+
+    """
+    # Perform fit
+    (aCoeff, bCoeff, rVal, _, _) = stats.linregress(x_array, y_array)
+
+    # Use fits to predict y output for a range of diameters
+    x_samples_array = np.linspace(min(x_array), max(x_array), 1000)
+    y_predict_array = aCoeff * x_samples_array + bCoeff
+
+    # Create a string, showing the form of the equation (with fitted coefficients) and r squared value.
+    # Coefficients are rounded to two decimal places.
+    equation = 'y = {} x + {} (R$^2$ = {})'.format(round(aCoeff,2), round(bCoeff,2), round(rVal**2,2))
+
+    plt.figure()
+
+    # Plot fit against original data
+    plt.plot(x_array, y_array,'.')
+    plt.plot(x_samples_array, y_predict_array)
+    plt.title('{} vs {}'.format(x_label, y_label))
+
+    x_label = 'x = {}'.format(x_label)
+    plt.xlabel(x_label)
+    y_label = 'y = {}'.format(y_label)
+    plt.ylabel(y_label)
+
+    plt.legend(['x vs y', equation])
+
+    # Save plot
+    if plot_filepath is None:
+        plt.show()
+    else:
+        plt.savefig(plot_filepath, dpi=200, format='PNG')
+        plt.close()
+        
+
+def color_MTG_Nitrogen(g, df, t, SCREENSHOT_DIRPATH):
+    
+
+    def color_map(N):
+        if 0 <= N <= 0.5: # TODO: organe senescent (prendre prop)
+            color_map = [150, 100, 0]
+        elif 0.5 < N < 5: # Fvertes
+            color_map = [int(255 - N*51), int(255 - N*20), 50]
+        else:
+            color_map = [0,155,0]
+        return color_map
+
+    def calculate_Total_Organic_Nitrogen(amino_acids, proteins, Nstruct):
+        """Total amount of organic N (amino acids + proteins + Nstruct).
+
+        :Parameters:
+            - `amino_acids` (:class:`float`) - Amount of amino acids (µmol N)
+            - `proteins` (:class:`float`) - Amount of proteins (µmol N)
+            - `Nstruct` (:class:`float`) - Structural N mass (g)
+        :Returns:
+            Total amount of organic N (mg)
+        :Returns Type:
+            :class:`float`
+        """
+        return (amino_acids + proteins)*14E-3 + Nstruct*1E3
+
+    colors = {}
+
+    groups_df = df.groupby(['plant', 'axis', 'metamer', 'organ', 'element'])
+    for vid in g.components_at_scale(g.root, scale=5):
+        pid = int(g.index(g.complex_at_scale(vid, scale =1)))
+        axid = g.property('label')[g.complex_at_scale(vid, scale =2)]
+        mid = int(g.index(g.complex_at_scale(vid, scale =3)))
+        org = g.property('label')[g.complex_at_scale(vid, scale =4)]
+        elid = g.property('label')[vid]
+        id_map = (pid, axid, mid, org, elid)
+        if groups_df.groups.has_key(id_map):
+            N = (g.property('proteins')[vid]*14E-3) / groups_df.get_group(id_map)['mstruct'].iloc[0]
+            #N = (calculate_Total_Organic_Nitrogen(g.property('amino_acids')[vid], g.property('proteins')[vid], g.property('Nstruct')[vid])) / g.property('mstruct')[vid]
+            colors[vid] = color_map(N)
+        else:
+            g.property('geometry')[vid] = None
+
+    # plantgl
+    s = to_plantgl(g, colors=colors)[0]
+    Viewer.add(s)
+    Viewer.camera.setPosition(Vector3(83.883,12.3239,93.4706))
+    Viewer.camera.lookAt(Vector3(0.,0,50))
+    Viewer.saveSnapshot(os.path.join(SCREENSHOT_DIRPATH, 'Day_{}.png'.format(t/24+1)))
+        
