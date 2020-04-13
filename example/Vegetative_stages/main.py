4c1c48e46f81a5b9daebe1d0755731fb9d4bea37
mngauthier <gauth.marion@gmail.com>
2020-04-13 10:56:04 +0200

    change run options

--- example/Vegetative_stages/main.py
+++ example/Vegetative_stages/main.py
@@ -978,7 +978,7 @@ def main(stop_time, forced_start_time=0, run_simu=True, run_postprocessing=True,
 
 
 if __name__ == '__main__':
-    main(2500, forced_start_time=0, run_simu=True, run_postprocessing=True, generate_graphs=True, run_from_outputs=True,
+    main(2500, forced_start_time=0, run_simu=True, run_postprocessing=True, generate_graphs=True, run_from_outputs=False,
          option_static=False, tillers_replications={'T1': 0.5, 'T2': 0.5, 'T3': 0.5, 'T4': 0.5},
          heterogeneous_canopy=True, N_fertilizations={2016: 357143, 2520: 1000000},
          PLANT_DENSITY={1: 250}, update_parameters_all_models=None,
