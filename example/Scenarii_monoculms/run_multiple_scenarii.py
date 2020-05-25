0a837f34a6a8f105568afb443b2d2d98409269c2
mngauthier <gauth.marion@gmail.com>
2020-05-25 14:51:09 +0200

    Run rearrangent of graphs and postprocessing only if the scenarii list ask for graph and postprocessing generation

--- example/Scenarii_monoculms/run_multiple_scenarii.py
+++ example/Scenarii_monoculms/run_multiple_scenarii.py
@@ -20,11 +20,13 @@ if __name__ == '__main__':
     p.terminate()
     p.join()
 
-    rearrange_graphs.rearrange_graphs(scenarii=list(scenarii))
-    rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['axes_postprocessing'], t=1999, scenarii=list(scenarii))
-    rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['axes_postprocessing'], t=2999, scenarii=list(scenarii))
-    rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['performance_indices'], scenarii=list(scenarii))
-    rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['leaf_traits'], scenarii=list(scenarii))
+    if 'Generate_Graphs' in scenarii_df.columns and any(scenarii_df.Generate_Graphs) :
+        rearrange_graphs.rearrange_graphs(scenarii=list(scenarii))
+    if 'Run_Postprocessing' in scenarii_df.columns and any(scenarii_df.Generate_Graphs):
+        rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['axes_postprocessing'], t=1999, scenarii=list(scenarii))
+        rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['axes_postprocessing'], t=3499, scenarii=list(scenarii))
+        rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['performance_indices'], scenarii=list(scenarii))
+        rearrange_postprocessing.rearrange_postprocessing(postprocessing_tables=['leaf_traits'], scenarii=list(scenarii))
 
     tend = time.time()
     tmp = (tend - tstart) / 60.
